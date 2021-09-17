e3a65f0a56e75f8b4d93750d469741cbc2d6df50
Tanaka Seigo <seigo@1ft-seabass.jp>
2021-09-17 11:54:03 +0900

    Bootstrap ビルド

--- vue-node-red-view/src/App.vue
+++ vue-node-red-view/src/App.vue
@@ -1,20 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <NodeRED/>
+  </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NodeRED from './components/NodeRED.vue'
+ 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NodeRED
   }
 }
 </script>
-
+ 
 <style>
+/*
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +25,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+*/
 </style>
---
+++ vue-node-red-view/src/components/NodeRED.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="row">
+    <div class="row">
+      <div class="col-sm">
+        <h1>{{infoTitle}}</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        <ul class="list-group">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+          <li class="list-group-item">A fourth item</li>
+          <li class="list-group-item">And a fifth one</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'NodeRED',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      infoTitle: "Node-RED INDEX"
+    }
+  },
+  methods: {
+ 
+  }
+}
+</script>
+ 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+</style>
